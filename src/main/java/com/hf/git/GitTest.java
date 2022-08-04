e2ca793c450f8edf33737b2bab519cf65452b65a
hufeng <15921042980@126.com>
2022-08-04 15:59:17 +0800

    hot-fix commit2

--- src/main/java/com/hf/git/GitTest.java
+++ src/main/java/com/hf/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("1111111111111111");
         System.out.println("2222222222222222");
         System.out.println("33333333333333333");
+        System.out.println("hot-fix test");
     }
 }
