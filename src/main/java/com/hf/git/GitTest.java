3376fb88c1015895ee08a5d778bd64d4b557f8e3
HF <15921042980@126.com>
2022-08-04 17:45:43 +0800

    pull commit1

--- src/main/java/com/hf/git/GitTest.java
+++ src/main/java/com/hf/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push...........");
         System.out.println("push2222222...........");
+        System.out.println("pull test");
     }
 }
